5a3ab62fb19a1e7632a085a6f04ebc20944dbec6
Ernest Walzel <ewalzel@gmail.com>
2014-07-22 16:29:34 +0100

    Basic form for updating (with validation)

--- forms.py
+++ forms.py
@@ -3,8 +3,8 @@ from wtforms import TextField, DateField
 from wtforms.validators import DataRequired
 
 DATE_FORMAT = '%Y-%m-%d'
+
 class EventForm(Form):
     id = TextField('Identifier', validators=[DataRequired()])
-    startdate = DateField('Start', validators=[DataRequired()], 
-                          format=DATE_FORMAT)
-    enddate = DateField('End', validators=[DataRequired()])
+    startdate = DateField('Start', format=DATE_FORMAT)
+    enddate = DateField('End', format=DATE_FORMAT)
